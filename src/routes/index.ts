67fcb43a9c9bdc22e141777a429e03fca4a33cbc
José Henrique <zehenrique0822@gmail.com>
2023-03-01 20:29:36 -0300

    chore: add eslint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from "express"
 
-const routes = Router();
+const routes = Router()
 
 routes.use('/', (req, res) => {
   res.send({ api: 'safe-collection-api' })
 })
 
-export default routes;
+export default routes
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from "express";
-import routes from "./routes";
+import express from "express"
+import routes from "./routes"
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(routes);
+app.use(express.json())
+app.use(routes)
 
 app.listen(process.env.API_PORT ?? 3333, () => { console.log('🔥 Server started at http://localhost:3333') })
