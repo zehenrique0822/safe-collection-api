f48dd1b987d1a9bb5bb0b2f1fc853f5610f9565d
José Henrique <zehenrique0822@gmail.com>
2023-03-04 01:04:55 -0300

    refactor: rename parameters router

--- src/routes/parameters/parameters.routes.ts
+++ src/routes/parameters/parameters.routes.ts
@@ -1,19 +1,19 @@
 import { Router } from 'express'
-import { CreateParametersController } from '../../modules/parameters/useCases/create/CreateParamatersController'
+import { CreateParametersController } from '../../modules/parameters/useCases/create/CreateParametersController'
 import { DeleteParametersController } from '../../modules/parameters/useCases/delete/DeleteParametersController'
 import { ListParametersController } from '../../modules/parameters/useCases/list/ListParametersController'
 import { UpdateParametersController } from '../../modules/parameters/useCases/update/UpdateParametersController'
 
-const parameteresRouter = Router()
+const parametersRouter = Router()
 
 const createParametersController = new CreateParametersController()
 const listParametersController = new ListParametersController()
 const updateParametersController = new UpdateParametersController()
 const deleteParametersController = new DeleteParametersController()
 
-parameteresRouter.post('/new', createParametersController.handle)
-parameteresRouter.get('/', listParametersController.handle)
-parameteresRouter.put('/edit/:id', updateParametersController.handle)
-parameteresRouter.delete('/delete/:id', deleteParametersController.handle)
+parametersRouter.post('/new', createParametersController.handle)
+parametersRouter.get('/', listParametersController.handle)
+parametersRouter.put('/edit/:id', updateParametersController.handle)
+parametersRouter.delete('/delete/:id', deleteParametersController.handle)
 
-export default parameteresRouter
+export default parametersRouter
